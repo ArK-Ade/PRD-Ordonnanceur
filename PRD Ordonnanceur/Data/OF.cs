19061fa94d8f24f3d504b4ad92eac7207a8f42bc
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-01-19 14:59:17 +0100

    Initial Commit

---
+++ PRD Ordonnanceur/Data/OF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRD_Ordonnanceur.Data
+{
+    class OF
+    {
+
+        public string[] step_sequence;
+
+        public OF() { }
+
+    }
+}
