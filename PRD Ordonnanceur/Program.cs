2b7f4c04ff80aec03c9a04112ac27d5bdc4e3002
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-01-19 14:50:27 +0100

    Initial commit

--- PRD Ordonnanceur/Program.cs
+++ PRD Ordonnanceur/Program.cs
@@ -1,3 +1,4 @@
+using PRD_Ordonnanceur.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace PRD_Ordonnanceur
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ImportForm());
         }
     }
 }
