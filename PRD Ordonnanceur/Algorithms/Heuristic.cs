4e985a8b10c9ad44df647ce08e0d1034390fd6bb
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-01-21 21:39:19 +0100

    Adding Unit Test

--- PRD Ordonnanceur/Algorithms/Heuristic.cs
+++ PRD Ordonnanceur/Algorithms/Heuristic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRD_Ordonnanceur.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,75 @@ using System.Threading.Tasks;
 
 namespace PRD_Ordonnanceur.Algorithms
 {
-    class Heuristic : IHeuristic
+    public class Heuristic : IHeuristic
     {
         public Heuristic()
         {
+
         }
 
+        public bool application()
+        {
+            return true;
+        }
+
+        int Smallest_index_DTI(OF[] OFs)
+        {
+
+            int Index = -1;
+            DateTime date = new DateTime(5000,01,01);
+            int count = 0;
+
+            foreach (OF oF in OFs)
+            {
+                if (date > oF.earliest_date)
+                {
+                    date = oF.earliest_date;
+                    Index = count;
+                }
+                count++;
+            }
+
+            return Index;
+        }
+
+        OF[] SortCrescentDtiCrescentDli(OF[] OFs)
+        {
+
+            OF[] tableau = OFs;
+            Array.Clear(tableau,0,tableau.Length);
+
+            OF earliest_OF = new();
+
+            int index;
+
+            for (int i = 0; i < OFs.Length; i++)
+            {
+                index = Smallest_index_DTI(OFs);
+                tableau[i] = OFs[index];
+
+                // TODO supprimer l'élément du tableau 
+                OFs[index].earliest_date = DateTime.MaxValue;
+            }
+
+            return tableau;
+        }
+
+        void Sort_crescent_dti_decendant_dli(OF[] OFs)
+        {
+            foreach (OF i in OFs)
+            {
+
+            }
+        }
+
+        void Sort_random_5_crescent(OF[] OFs)
+        {
+            foreach (OF i in OFs)
+            {
+
+            }
+        }
 
     }
 }
---
+++ PRD OrdonnanceurTests1/Algorithms/HeuristicTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PRD_Ordonnanceur.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRD_Ordonnanceur.Algorithms.Tests
+{
+    [TestFixture()]
+    public class HeuristicTests
+    {
+        [Test()]
+        public void HeuristicTest()
+        {
+            Assert.True(true);
+        }
+
+        [Test()]
+        public void applicationTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
