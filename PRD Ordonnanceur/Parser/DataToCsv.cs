f4f8dd40cf805b5ad72b961cbf5707c7e67b02ce
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-04-01 20:22:00 +0200

    upgrade DataToCsv

--- PRD Ordonnanceur/Parser/DataToCsv.cs
+++ PRD Ordonnanceur/Parser/DataToCsv.cs
@@ -22,20 +22,21 @@ namespace PRD_Ordonnanceur.Parser
             string nameFile = "/Operator.csv";
             using StreamWriter streamWriter = new(path + nameFile);
             using CsvWriter csvWriter = new(streamWriter, CultureInfo.CurrentCulture);
+            csvWriter.WriteField("ID");
             csvWriter.WriteField("Job Start");
             csvWriter.WriteField("Job End");
             csvWriter.WriteField("Code");
-            csvWriter.WriteField("ID");
+            
             csvWriter.NextRecord();
 
             foreach (var planning in plannings)
             {
                 foreach (var list in planning.PlanningOperator)
                 {
+                    csvWriter.WriteField<int>((int)list[4]);
                     csvWriter.WriteField<DateTime>((DateTime)list[1]);
                     csvWriter.WriteField<DateTime>((DateTime)list[2]);
                     csvWriter.WriteField((string)list[3]);
-                    csvWriter.WriteField<int>((int)list[4]);
                     csvWriter.NextRecord();
                 }
             }
@@ -48,7 +49,31 @@ namespace PRD_Ordonnanceur.Parser
         /// <param name="path"></param>
         public static void ParsingDataMachine(List<SolutionPlanning> plannings, string path)
         {
-            // Method intentionally left empty.
+            string nameFile = "/Machines.csv";
+            using StreamWriter streamWriter = new(path + nameFile);
+            using CsvWriter csvWriter = new(streamWriter, CultureInfo.CurrentCulture);
+            csvWriter.WriteField("UID Machine");
+            csvWriter.WriteField("Job Start");
+            csvWriter.WriteField("Job End");
+            csvWriter.WriteField("UID OF");
+            csvWriter.WriteField("UID Beginning Operator");
+            csvWriter.WriteField("UID End Operator");
+            csvWriter.NextRecord();
+
+            foreach (var planning in plannings)
+            {
+                foreach (var list in planning.PlanningOperator)
+                {
+                    csvWriter.WriteField<int>((int)list[7]);
+                    csvWriter.WriteField<DateTime>((DateTime)list[1]);
+                    csvWriter.WriteField<DateTime>((DateTime)list[2]);
+                    csvWriter.WriteField<int>((int)list[3]);
+                    csvWriter.WriteField<int>((int)list[4]);
+                    csvWriter.WriteField<int>((int)list[5]);
+                    csvWriter.WriteField<int>((int)list[6]);
+                    csvWriter.NextRecord();
+                }
+            }
         }
 
         /// <summary>
@@ -58,7 +83,22 @@ namespace PRD_Ordonnanceur.Parser
         /// <param name="path"></param>
         public static void ParsingDataConsumable(List<SolutionPlanning> plannings, string path)
         {
-            // Method intentionally left empty.
+            string nameFile = "/Consumable.csv";
+            using StreamWriter streamWriter = new(path + nameFile);
+            using CsvWriter csvWriter = new(streamWriter, CultureInfo.CurrentCulture);
+            csvWriter.WriteField("UID Consumable");
+            csvWriter.WriteField("Quantity Used");
+            csvWriter.NextRecord();
+
+            foreach (var planning in plannings)
+            {
+                foreach (var list in planning.PlanningOperator)
+                {
+                    csvWriter.WriteField<int>((int)list[1]);
+                    csvWriter.WriteField<double>((double)list[2]);
+                    csvWriter.NextRecord();
+                }
+            }
         }
 
         /// <summary>
@@ -68,7 +108,26 @@ namespace PRD_Ordonnanceur.Parser
         /// <param name="path"></param>
         public static void ParsingDataOF(List<SolutionPlanning> plannings, string path)
         {
-            // Method intentionally left empty.
+            string nameFile = "/OFs.csv";
+            using StreamWriter streamWriter = new(path + nameFile);
+            using CsvWriter csvWriter = new(streamWriter, CultureInfo.CurrentCulture);
+            csvWriter.WriteField("Date");
+            csvWriter.WriteField("UID Tank");
+            csvWriter.WriteField("UID Machine");
+            csvWriter.WriteField("");
+            csvWriter.WriteField("Quantity Used");
+            csvWriter.WriteField("Quantity Used");
+            csvWriter.NextRecord();
+
+            foreach (var planning in plannings)
+            {
+                foreach (var list in planning.PlanningOperator)
+                {
+                    csvWriter.WriteField<int>((int)list[1]);
+                    csvWriter.WriteField<double>((double)list[2]);
+                    csvWriter.NextRecord();
+                }
+            }
         }
 
         /// <summary>
@@ -78,7 +137,28 @@ namespace PRD_Ordonnanceur.Parser
         /// <param name="path"></param>
         public static void ParsingDataTank(List<SolutionPlanning> plannings, string path)
         {
-            // Method intentionally left empty.
+            string nameFile = "/Tanks.csv";
+            using StreamWriter streamWriter = new(path + nameFile);
+            using CsvWriter csvWriter = new(streamWriter, CultureInfo.CurrentCulture);
+            csvWriter.WriteField("UID Tank");
+            csvWriter.WriteField("Job Start");
+            csvWriter.WriteField("Job End");
+            csvWriter.WriteField("UID OF");
+            csvWriter.WriteField("UID Operator");
+            csvWriter.NextRecord();
+
+            foreach (var planning in plannings)
+            {
+                foreach (var list in planning.PlanningOperator)
+                {
+                    csvWriter.WriteField<int>((int)list[6]);
+                    csvWriter.WriteField<DateTime>((DateTime)list[1]);
+                    csvWriter.WriteField<DateTime>((DateTime)list[2]);
+                    csvWriter.WriteField<int>((int)list[4]);
+                    csvWriter.WriteField<int>((int)list[5]);
+                    csvWriter.NextRecord();
+                }
+            }
         }
     }
 }
